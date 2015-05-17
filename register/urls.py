e504a87a6f8c6244184934420518d4574ab5f06a
Tyler Agee <tyler@pyroturtle.com>
2015-05-17 04:35:23 -0700

    basic bootstrap functions

--- register/urls.py
+++ register/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^/', 'recipie.views.index'),
+    url(r'^', 'recipie.views.index'),
 ]
